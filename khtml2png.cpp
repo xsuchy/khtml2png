b2efb66c588f930cfa7b7a7ceba7bab1ff1178f7
Miroslav Suchy <mirek@triple.(none)>
2009-05-18 14:18:44 +0200

    port to KDE4
    credits go to Dilshod Mukhtarov

--- khtml2png.cpp
+++ khtml2png.cpp
@@ -2,6 +2,7 @@
     Heavily based on KDE HTML thumbnail creator
     Copyright (C) 2003 Simon MacMullen
     Copyright (C) 2004 Miroslav Suchy
+    Copyright (C) 2009 Dilshod Mukhtarov
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public
@@ -26,12 +27,12 @@
 #include <qimage.h>
 #include <qpainter.h>
 
-#include <kapplication.h>
+#include <KApplication>
 #include <khtml_part.h>
-#include <khtmlview.h>
-#include <kcmdlineargs.h>
-#include <klocale.h>
-#include <kaboutdata.h>
+#include <KHTMLView>
+#include <KCmdLineArgs>
+#include <KLocale>
+#include <KAboutData>
 #include <iostream>
 #include <string.h>
 #include <dom/dom_core.h>
@@ -60,16 +61,16 @@ KHTML2PNG::KHTML2PNG(int jscript, int java, int plugin, int refresh, int localon
 }
 
 QImage* KHTML2PNG::create(const QString &path, int width, int height, int time, int flashDelay) {
-    m_html->openURL(path);
+    m_html->openUrl(path);
     m_html->view()->resize(width, height);
 
     m_completed = false;
-    startTimer(time * 1000);
+    int timerId = startTimer(time * 1000);
     while (!m_completed) {
 		usleep(1);
         	kapp->processEvents();
 	}
-	killTimers();
+	killTimer(timerId);
 	flashflag = false;
    	showTree(m_html->document());
     // Do a screengrab type thing - we get form widgets that way
@@ -79,22 +80,22 @@ QImage* KHTML2PNG::create(const QString &path, int width, int height, int time, 
 		m_html->view()->show();
 		// Wait for the flash movie to play
 	        m_flashStarted = false;
-        	startTimer(flashDelay > 0 ? flashDelay * 1000 : 3000);
+        	timerId = startTimer(flashDelay > 0 ? flashDelay * 1000 : 3000);
         	while (!m_flashStarted) {
 			usleep(1);
         		kapp->processEvents();
 		}
-        	killTimers();
-	        pix = QPixmap::grabWindow(m_html->view()->clipper()->winId());
+        	killTimer(timerId);
+	        pix = QPixmap::grabWindow(m_html->view()->viewport()->winId());
 	} else {
 		m_html->view()->hide();
 		// Grab using Qt - doesn't work if window contains non-qt widgets
-        	pix = QPixmap::grabWidget(m_html->view()->clipper());
+        	pix = QPixmap::grabWidget(m_html->view()->viewport());
 	}
 
-    QImage *image = new QImage(pix.convertToImage());
+    QImage *image = new QImage(pix.toImage());
     // do not spend time with active items, render blank page and wait for others
-    m_html->closeURL();
+    m_html->closeUrl();
 
     return image;
 }
@@ -156,23 +157,6 @@ void KHTML2PNG::showRecursive(const DOM::Node &node) {
   }
 }
 
-static KCmdLineOptions options[] = {
-	{ "width ", "Width of canvas on which to render html", "800" },
-	{ "height ", "Height of canvas on which to render html", "1000" },
-	{ "scaled-width ", "Width of image to produce", "80" },
-	{ "scaled-height ", "Height of image to produce", "100" },
-	{ "flash-delay ", "Pause after loading page to allow flash movie to fade up (-1 mean no flash)", "3" },
-	{ "time ", "Maximum time in seconds to spend loading page", "30" },
-	{ "nojscript", "Disable JavaScript", 0},
-	{ "nojava", "Disable Java", 0},
-	{ "noplugin", "Disable plugins", 0 },
-	{ "norefresh", "Disable meta refresh", 0 },
-	{ "localonly", "Disable local references", 0},
-	{ "+url ", "URL of page to render", 0 },
-	{ "+outfile ", "Output file", 0 },
-	{ 0, 0, 0 }
-};
-
 void KHTML2PNG::processBatch(const QString &fileName, const QString &path, int width, int height, int time, int flashDelay, int scaledWidth, int scaledHeight) {
 	QImage* renderedImage = this->create(path, width, height, time, flashDelay);
 
@@ -186,7 +170,7 @@ void KHTML2PNG::processBatch(const QString &fileName, const QString &path, int w
 		}
 
 	} else {
-		QImage scaledImage = renderedImage->smoothScale(scaledWidth, scaledHeight);
+		QImage scaledImage = renderedImage->scaled(scaledWidth, scaledHeight);
 
 		if (fileName.endsWith(".jpeg") || fileName.endsWith(".jpg")) {
 			scaledImage.save(fileName, "JPEG");
@@ -198,35 +182,50 @@ void KHTML2PNG::processBatch(const QString &fileName, const QString &path, int w
 }
 
 int main(int argc, char **argv) {
-	KAboutData aboutData("khtml2png", I18N_NOOP("KHTML2PNG"),
+	KCmdLineOptions options;
+	options.add( "width ", ki18n("Width of canvas on which to render html"), "800" );
+	options.add( "height ", ki18n("Height of canvas on which to render html"), "1000" );
+	options.add( "scaled-width ", ki18n("Width of image to produce"), "80" );
+	options.add( "scaled-height ", ki18n("Height of image to produce"), "100" );
+	options.add( "flash-delay ", ki18n("Pause after loading page to allow flash movie to fade up (-1 mean no flash)"), "3" );
+	options.add( "time ", ki18n("Maximum time in seconds to spend loading page"), "30" );
+	options.add( "nojscript", ki18n("Disable JavaScript") );
+	options.add( "nojava", ki18n("Disable Java") );
+	options.add( "noplugin", ki18n("Disable plugins") );
+	options.add( "norefresh", ki18n("Disable meta refresh") );
+	options.add( "localonly", ki18n("Disable local references") );
+	options.add( "+url ", ki18n("URL of page to render") );
+	options.add( "+outfile ", ki18n("Output file") );
+
+	KAboutData aboutData("khtml2png", "KHTML2PNG", ki18n("khtml2png"),
 		"1.0.3",
-		I18N_NOOP("Render HTML to a PNG from the command line\nExample: khtml2png --width 800 --height 1000 --scaled-width 800 --scaled-height 1000 http://www.kde.org/ kde-org.png\nor khtml2png --width 800 --height 1000 - and write url on first line of stdin and on second 'filename scaledwidth scaledheight flash'"),
+		ki18n("Render HTML to a PNG from the command line\nExample: khtml2png --width 800 --height 1000 --scaled-width 800 --scaled-height 1000 http://www.kde.org/ kde-org.png\nor khtml2png --width 800 --height 1000 - and write url on first line of stdin and on second 'filename scaledwidth scaledheight flash'"),
 		KAboutData::License_GPL,
-		"(c) 2003 Simon MacMullen.");
-	aboutData.addAuthor("Simon MacMullen", 0, "s.macmullen@ry.com");
-	aboutData.addAuthor("Miroslav Suchy", 0, "miroslav@suchy.cz");
+		ki18n("(c) 2003 Simon MacMullen."));
+	aboutData.addAuthor(ki18n("Simon MacMullen"), ki18n(0), "s.macmullen@ry.com");
+	aboutData.addAuthor(ki18n("Miroslav Suchy"), ki18n(0), "miroslav@suchy.cz");
+        aboutData.addAuthor(ki18n("Dilshod Mukhtarov"), ki18n(0), "dilshodm at gmail dot com");
 	KCmdLineArgs::init(argc, argv, &aboutData);
 	KCmdLineArgs::addCmdLineOptions(options);
 	KCmdLineArgs *args=KCmdLineArgs::parsedArgs();
 
-	int width = QString::fromLatin1(args->getOption("width")).toInt();
-	int height = QString::fromLatin1(args->getOption("height")).toInt();
-	int scaledWidth = QString::fromLatin1(args->getOption("scaled-width")).toInt();
-	int scaledHeight = QString::fromLatin1(args->getOption("scaled-height")).toInt();
-	int time = QString::fromLatin1(args->getOption("time")).toInt();
+	int width = args->getOption("width").toInt();
+	int height = args->getOption("height").toInt();
+	int scaledWidth = args->getOption("scaled-width").toInt();
+	int scaledHeight = args->getOption("scaled-height").toInt();
+	int time = args->getOption("time").toInt();
 
     int flashDelay = -1;
 
     if (args->isSet("flash-delay")){
-        flashDelay = QString::fromLatin1(args->getOption("flash-delay")).toInt();
+        flashDelay = args->getOption("flash-delay").toInt();
     }
 
-	KInstance inst(&aboutData);
 	KApplication app;
 	char Input [2000];
 	char ids [2000] = "-";
 	if (args->count() != 2) {
-		if (args->count() == 0 || strcmp(args->arg(0),ids)!=0)  {
+		if (args->count() == 0 || strcmp(args->arg(0).toAscii().data(),ids)!=0)  {
 		    args->usage();
 		}
 	}
@@ -236,12 +235,12 @@ int main(int argc, char **argv) {
 			args->isSet("refresh"),
 			args->isSet("localonly"));
     
-	if (strcmp(args->arg(0),ids)==0) {
+	if (strcmp(args->arg(0).toAscii().data(),ids)==0) {
 		while (scanf(" %[^\n]",  Input) != EOF && scanf(" %s", ids) != EOF 
 			&& scanf(" %d", &scaledWidth) != EOF && scanf(" %d", &scaledHeight)
 			&& scanf(" %d", &flashDelay)) {
 			convertor->processBatch(ids, Input, width, height, time, flashDelay, scaledWidth, scaledHeight);
-			printf ("Thumbnail from %s saved to %s (flash: %s; last modified: %s).\n", Input, ids, convertor->haveFlash()?"yes":"no", convertor->lastModified().isNull()?"":convertor->lastModified().ascii());
+			printf ("Thumbnail from %s saved to %s (flash: %s; last modified: %s).\n", Input, ids, convertor->haveFlash()?"yes":"no", convertor->lastModified().isNull()?"":convertor->lastModified().toAscii().data());
 			fflush(NULL);
 		}
 	} else {
